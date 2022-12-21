782ca3ad9b5e1b6fd6647518ec5fb533d305a25e
justinwling <justinwling@gmail.com>
2022-12-21 11:49:36 -0800

    Initial pytest unit testing for get_CDSpams.py

--- backend/test_get_CDSpams.py
+++ backend/test_get_CDSpams.py
@@ -8,30 +8,44 @@
 #  5. backend.get_ref_PAM_seq
 #  6. backend.subset_CDSpams
 
-from get_CDSpams import getCDSpams
+import pytest
+from . import configs
+from .get_CDSpams import get_CDS_df, get_CDS_seq, get_PAM_coords
 
 def test_get_CDS_df():
-    #Test for: sequence end (end) - sequence start (start) = sequence length (strand)
-    
-    #Test for: Variable 'end' is greater than 'start'
-    pass
+    cds_df = get_CDS_df(db_path=configs.GTF_DB_PATH)
+    #Test that sequence end (end) - sequence start (start) is a positive integer
+    CDS_df_rows=cds_df.shape[0]
+    for x in range(CDS_df_rows):
+        print ("row: ",x)
+        assert ((((cds_df.iloc[x]['end'])-(cds_df.iloc[x]['start'])))>0)
 
-def test_get_CDS_seq():
-    #Test that only Chr1-22, chrX, CDS in pam_df
+    return cds_df
 
-    #Test that PAM columns were added to pam_df.csv
-    pass
 
-def test_get_PAM_coords():
+def test_get_CDS_seq(cds_df=test_get_CDS_df()):
+    #Test that the sequence added to df is the correct length based on start and end locations using 
+    cds_df = get_CDS_seq(df=cds_df, genome_path=configs.GENOME_FA_PATH)
+
+    CDS_df_rows=cds_df.shape[0]
+    for x in range(CDS_df_rows):
+        assert ((((cds_df.iloc[x]['end'])-(cds_df.iloc[x]['start']))) == len(cds_df.iloc[x]['seq'])) 
+
+    return cds_df
+
+def test_get_PAM_coords(cds_df=test_get_CDS_seq()):
+
+    #Test if PAM coordinates point to NGG nucleotides
+    pam_df = get_PAM_coords(df=cds_df)
     #
     pass
 
-def test_make_CDSpamsbed():
-    pass
+#def test_make_CDSpamsbed():
+#    pass
 
-def test_get_ref_PAM_seq():
-    pass
+#def test_get_ref_PAM_seq():
+#    pass
 
-def test_subset_CDSpams():
-    pass
+#def test_subset_CDSpams():
+#    pass
 
